6c093cfab736cf358d2e98dc6bae0c71495a3f0a
Dhananjay <dhananjaypatel.official@gmail.com>
2021-06-15 19:59:54 +0530

    Done

--- demo.py
+++
@@ -1,7 +0,0 @@
-import re
-text = "Please contact us at contact@tutorialspoint.com for further information."+\
-        " You can also give feedbacl at feedback@tp.com"
-
-
-emails = re.findall("[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", text)
-print(emails)
